da6b8b19ad7bee1b0c0dd01d608b16ed5c2f5720
Ang Shi Ya <>
2019-10-12 18:09:24 +0800

    Add bullets

---
+++ Assets/Scripts/BulletManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour {
+    public GameObject bullet;
+    public Vector3 offScreen = new Vector3(-10, 0, 0);
+    public int numBullets = 100;
+
+    private GameObject[] bullets; 
+    private int nextBulletIndex;
+
+    // Start is called before the first frame update
+    void Start() {
+        bullets = new GameObject[numBullets];
+        nextBulletIndex = 0;
+        for (int i = 0; i < numBullets; i++) {
+            bullets[i] = Instantiate(bullet, offScreen, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    public GameObject getBullet() {
+        nextBulletIndex %= numBullets;
+        return bullets[nextBulletIndex++];
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,29 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+    public BulletManager bulletManager;
+    public float cooldown = 0.1f;
+
     private bool isMouseDown;
     private Vector2 clickPosition;
     private Vector2 currentPosition;
+    private float timer = 0;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0)) 
-        {
+    void Update() {
+        move();
+
+        timer += Time.deltaTime;
+        if (timer > cooldown) {
+            attack();
+            timer = 0;
+        }
+    }
+
+    private void move() {
+        if (Input.GetMouseButtonDown(0)) {
             clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             currentPosition = transform.position;
             isMouseDown = true;
         } 
-        else if (Input.GetMouseButtonUp(0)) 
-        {
+        else if (Input.GetMouseButtonUp(0)) {
             isMouseDown = false;
         }
 
@@ -33,5 +42,11 @@ public class PlayerMove : MonoBehaviour
             Vector2 offset = position - clickPosition;
             transform.position = currentPosition + offset;
         }
+    }
+
+    private void attack() {
+        GameObject bullet = bulletManager.getBullet();
+        bullet.transform.position = transform.position;
+        bullet.GetComponent<Bullet>().setVelocity(10);
     }
 }
